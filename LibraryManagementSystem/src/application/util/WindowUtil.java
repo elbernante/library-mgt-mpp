c25358ce95cd3754a7f41cc7dc28b206dbdd8d89
Peter James Bernante <elbernante@gmail.com>
2017-03-08 17:20:50 -0600

    Add loadWindow with callback function

--- LibraryManagementSystem/src/application/util/WindowUtil.java
+++ LibraryManagementSystem/src/application/util/WindowUtil.java
@@ -3,6 +3,7 @@ package application.util;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -24,10 +25,25 @@ public class WindowUtil {
 		}
 	}
 	
+	public static void loadWindow(String loc, String title, boolean resizable, FxmlLoadCallback callback) {
+		try {
+			FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemClassLoader().getResource("application/" + loc));
+			
+			Parent parent = loader.load();
+			Stage stage = new Stage(StageStyle.DECORATED);
+			stage.setTitle(title);
+			stage.setScene(new Scene(parent));
+			stage.setResizable(resizable);
+			callback.callback(loader);
+			stage.show();
+		} catch (IOException ex) {
+			Logger.getLogger(WindowUtil.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+	}
+	
 	public static void messageBox(String message) {
-		// TODO: show alert message
-		System.out.println("Message box: " + message);
-		
+		new Alert(Alert.AlertType.INFORMATION, message).showAndWait();
 	}
 
 }
